0a4cf380d8f56bce72d6d829edecdba45ca02944
ryozo42-dev-app <ryozo.42@gmail.com>
2026-04-22 01:30:02 +0900

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      firebase: false,
+      "firebase-functions": false,
+    }
+    return config
+  },
 };
 
 export default nextConfig;
